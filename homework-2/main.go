4ff31de2d063108830739a282beee3a5fddf0f7b
Aleksey Gusev <audetv@gmail.com>
2020-09-29 15:12:31 +0300

    Added solutions for 1-3 tasks

---
+++ homework-2/main.go
@@ -0,0 +1,74 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var num int
+
+	defer func() {
+		if r := recover(); r != nil {
+			fmt.Println(r)
+		}
+	}()
+
+	fmt.Println("Введите целое число: ")
+	_, err := fmt.Scanf("%d", &num)
+
+	if err != nil {
+		panic(fmt.Sprintf("Вы ввели некорректное число, работа программы остановлена. %v", err))
+	}
+
+	doHomework(num)
+}
+
+func doHomework(num int) {
+
+	// Задача №1.
+
+	fmt.Println(isEven(num))
+
+	// Задача №2.
+
+	message := "Это число не делится на 3 без остатка"
+
+	if isMultipleOf(3, num) {
+		message = "Это число делится на 3 без остатка"
+	}
+	fmt.Println(message)
+
+	// Задача №3.
+	fmt.Println("100 первых чисел Фибоначчи: ")
+
+	fibonacci(100)
+
+}
+
+func isEven(num int) (message string) {
+	message = "Вы ввели нечетное число"
+	if isMultipleOf(2, num) {
+		message = "Вы ввели четное число"
+	}
+	return
+}
+
+// isMultipleOf определяет, делится ли число num без остатка на value
+func isMultipleOf(value int, num int) bool {
+	if num%value == 0 {
+		return true
+	}
+	return false
+}
+
+func fibonacci(i int) {
+	var a int64 = 0
+	var b int64 = 1
+	for n := 0; n < i; n++ {
+		a, b = b, a+b
+		if n == 0 {
+			a = 0
+		}
+		fmt.Printf("Фибоначчи %v = %v\n", n, a)
+	}
+}
