325ac8bab97f2a136e00adf8989e39d8cc92bcaa
Aleksey Gusev <audetv@gmail.com>
2020-10-12 17:47:31 +0300

    Added calculator help function

--- homework-4/main.go
+++ homework-4/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"fmt"
+	"github.com/audetv/GoCource/homework-4/calculator"
 	"github.com/audetv/GoCource/homework-4/phones"
 	"github.com/audetv/GoCource/homework-4/shape"
 )
@@ -9,4 +11,36 @@ func main() {
 	shape.Process()
 
 	phones.Process()
+
+	calc()
+
+}
+
+func calc() {
+	input := ""
+	for {
+		fmt.Print("> ")
+		if _, err := fmt.Scanln(&input); err != nil {
+			fmt.Println(err)
+			continue
+		}
+
+		if input == "exit" {
+			break
+		}
+
+		// Функция, которая выводит справку по калькулятору.
+		if input == "help" {
+			fmt.Println("Справка по калькулятору")
+			fmt.Println("В разработке: текст справки, с описанием функций")
+			continue
+		}
+
+		if res, err := calculator.Calculate(input); err == nil {
+			fmt.Printf("Результат: %v\n", res)
+		} else {
+			fmt.Println("Не удалось произвести вычисление")
+		}
+	}
+
 }
